fcda7c3298fce34227b630f4a9a562958989f0e9
dimitri-hoareau <dimitrihoareau2@gmail.com>
2022-09-10 00:50:45 +0200

    get json response

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const {success} = require('./helper.js')
 let pokemons = require('./mock-pokemons')
 
 const app = express()
@@ -8,8 +9,17 @@ app.get('/', (req, res) => res.send('Hello, express'))
 app.get('/api/pokemons/:id', (req, res) => {
     const id = parseInt(req.params.id, 10)
     const pokemon = pokemons.find(pokemon => pokemon.id === id)
-    res.send(`Hello, pokemon ${pokemon.name} !`)
-
+    const message = 'Un polémon a bien été trouvé'
+    res.json(success(message, pokemon))
 })
+
+app.get('/api/pokemons', (req, res) => {
+    const message = `il y a ${pokemons.length} pokemons !`
+    res.json(success(message, pokemons))
+})
+
+// app.get('/api/pokemons', (req, res) => {
+//     res.send(`il y a ${pokemons.lengh} pokemons !`)
+// })
 
 app.listen(port, () => console.log(`application démarrée sur le port : ${port}`))
