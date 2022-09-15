14895e7187ebbca04943e23be824a56c5f0b2006
dimitri-hoareau <dimitrihoareau2@gmail.com>
2022-09-16 00:22:10 +0200

    add crud request

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
 const favicon = require('serve-favicon')
-const {success} = require('./helper.js')
+const {success, getUniqueId} = require('./helper.js')
 let pokemons = require('./mock-pokemons')
 
 const app = express()
@@ -10,6 +11,8 @@ const port  = 3000
 app
     .use(favicon(__dirname + '/favicon.ico'))
     .use(morgan('dev'))
+    .use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get('/', (req, res) => res.send('Hello, express'))
 
@@ -23,6 +26,33 @@ app.get('/api/pokemons/:id', (req, res) => {
 app.get('/api/pokemons', (req, res) => {
     const message = `il y a ${pokemons.length} pokemons !`
     res.json(success(message, pokemons))
+})
+
+app.post('/api/pokemons', (req, res) => {
+    const id = getUniqueId(pokemons) 
+    const pokemonCreated = { ...req.body, ...{id : id, created: new Date()}}
+    pokemons.push(pokemonCreated)
+    const message = `Le pokemon ${pokemonCreated.name} a été crée !`
+    res.json(success(message, pokemonCreated))
+})
+
+app.put('/api/pokemons/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const pokemonUpdated = { ...req.body, id : id}
+    pokemons = pokemons.map(pokemon => {
+        return pokemon.id === id ? pokemonUpdated : pokemon
+    })
+    const message = `Le pokemon ${pokemonUpdated.name} a été modifié!`
+    res.json(success(message, pokemonUpdated))
+})
+
+app.delete('/api/pokemons/:id', (req, res) => {
+
+    const id = parseInt(req.params.id)
+    const pokemonDeleted = pokemons.find(pokemon => pokemon.id === id)
+    pokemons.filter(pokemon => pokemon.id !== id)
+    const message = `Le pokemon ${pokemonDeleted.name} a été supprimé!`
+    res.json(success(message, pokemonDeleted))
 })
 
 app.listen(port, () => console.log(`application démarrée sur le port : ${port}`))
--- helper.js
+++ helper.js
@@ -2,3 +2,10 @@ exports.success = (message, data) => {
     return { message, data}
 }
 
+exports.getUniqueId = (pokemons) => {
+    const ponkemonsIds = pokemons.map(pokemon => pokemon.id)
+    const maxId = ponkemonsIds.reduce((a,b) => Math.max(a,b))
+    const uniqueId = maxId + 1
+
+    return uniqueId
+} 
